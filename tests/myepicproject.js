832e1493dd73eccb6bd278a7c4721ca19423c2d4
Jeffrey Tan <jeffreyotan@yahoo.com>
2021-12-16 23:24:37 +0800

    Initial commit

---
+++ tests/myepicproject.js
@@ -0,0 +1,51 @@
+const anchor = require('@project-serum/anchor');
+
+const { SystemProgram } = anchor.web3;
+
+const main = async () => {
+  console.log("Starting test...");
+
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.Myepicproject;
+
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
+
+  console.log("Your transaction signature: ", tx);
+
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("GIF count: ", account.totalGifs.toString());
+
+  await program.rpc.addGif("https://media.giphy.com/media/xUOwG1GXJvJlajETMk/giphy.gif", {
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+    },
+  });
+
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("GIF count: ", account.totalGifs.toString());
+  console.log("GIF list: ", account.gifList);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
